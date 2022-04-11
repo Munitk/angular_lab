e24a3a43c99ae83f18c5e66fe405d7dcdd1b0c34
munit <munitkumardbg@gnmail.com>
2022-04-11 16:45:43 +0530

    added new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadeComponent } from './components/heade/heade.component';
+import { CourseComponent } from './components/course/course.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListCourseComponent } from './components/list-course/list-course.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { ListContactComponent } from './components/list-contact/list-contact.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadeComponent,
+    CourseComponent,
+    HomeComponent,
+    ListCourseComponent,
+    MainContainerComponent,
+    ListContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
